fbda0c97a495a57d7254fbe451c5b1a7514fbcc3
Douglas Kiang <douglas.kiang@gmail.com>
2014-04-18 23:25:35 +0000

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pixster::Application.config.secret_key_base = 'f5839db8552cb2c40a57e6322153701290d9be5e3833e027e6890e32c86bdf3e043eb3a113ad48fa73c061aa83204baab078441ddc0440fcfcd91347b8956b81'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Pixster::Application.config.session_store :cookie_store, key: '_pixster_session'
